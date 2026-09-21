a9412fb92ffe10ececa81fc44aa9f94d5612f8da
christoffervilstrup <christoffer.vilstrup@gmail.com>
2026-09-21 14:29:31 +0200

    isPositive metoden er blevet mere simpel

--- src/CheckPositive.java
+++ src/CheckPositive.java
@@ -1,13 +1,9 @@
 /* isPositive(...) virker, men kan simplificeres lidt */
 
 void main() {
-    IO.println(isPositive(-5));
+    IO.println(isPositive(-4));
 }
 
 boolean isPositive(int number) {
-    if (number > 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return (number > 0);
 }
